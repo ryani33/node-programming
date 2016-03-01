982282f105b6a052d6ab6349c71452dcc524b85f
Xiaoyu Zhai <xiaoyu.zhai@hotmail.com>
2016-03-01 16:37:57 -0500

    go thru the requirements of lab4 and fix

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ var myData = require('./data.js');
 // This package exports the function to create an express instance:
 var app = express();
 
+app.set('view engine', 'ejs');
+
 // This is called 'adding middleware', or things that will help parse your request
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
@@ -17,30 +19,25 @@ app.use('/assets', express.static('static'));
 
 // Setup your routes here!
 
-function respond_result(response, res) {
-    try {
-        response.json({status: "success", "result": res});
-    } catch (e) {
-        response.status(500).json({status: "error", "message": e});
-    }
-}
-
 app.get("/", function (request, response) { 
-    // We have to pass a second parameter to specify the root directory
-    // __dirname is a global variable representing the file directory you are currently in
-    response.sendFile("./pages/index.html", { root: __dirname });
+    response.render('pages/index');
+});
+
+app.get("/calculate", function (request, response) { 
+    response.render('pages/calculate');
 });
 
 app.get("/api/perMonthRetirementSavings", function (request, response) { 
     if (request.query.years && request.query.perMonth && request.query.interestRate) {
     	try {
 	        var res = myData.retirementAmountIfSavingPerMonth(request.query.years, request.query.perMonth, request.query.interestRate);
-	        respond_result(response, res);
-	    } catch (e) {
-	        response.status(500).json({status: "error", "message": e});
-	    }
+	        response.render('pages/result', { operationTitle: "Monthly Retirement Savings", result: res,
+                type: 1, param1: request.query.years, param2:request.query.perMonth, param3:request.query.interestRate});
+        } catch (e) {
+            response.status(500).render('pages/error', { error: e});
+        }
     } else {
-    	response.status(500).json({status: "error", "message": "You need provide all three parameters!"});
+        response.status(500).render('pages/error', { error: "You need provide all three parameters!"});
     }
 });
 
@@ -48,12 +45,13 @@ app.get("/api/investedAmount", function (request, response) {
     if (request.query.years && request.query.initial && request.query.interestRate) {
         try {
 	        var res = myData.investedAmountAfterSomeYears(request.query.years, request.query.initial, request.query.interestRate);
-	        respond_result(response, res);
-	    } catch (e) {
-	        response.status(500).json({status: "error", "message": e});
-	    }
+	        response.render('pages/result', { operationTitle: "Invested Amount", result: res,
+                type: 2, param1: request.query.years, param2:request.query.initial, param3:request.query.interestRate});
+        } catch (e) {
+            response.status(500).render('pages/error', { error: e});
+        }
     } else {
-    	response.status(500).json({status: "error", "message": "You need provide all three parameters!"});
+        response.status(500).render('pages/error', { error: "You need provide all three parameters!"});
     }
 });
 
@@ -61,15 +59,57 @@ app.get("/api/loanPayoff", function (request, response) {
     if (request.query.monthlyAmount && request.query.loanAmount && request.query.interestRate) {
         try {
 	        var res = myData.monthsToPayOffLoan(request.query.monthlyAmount, request.query.loanAmount, request.query.interestRate);
-	        respond_result(response, res);
-	    } catch (e) {
-	        response.status(500).json({status: "error", "message": e});
-	    }
+	        response.render('pages/result', { operationTitle: "Loan Payoff", result: res,
+                type: 3, param1: request.query.monthlyAmount, param2:request.query.loanAmount, param3:request.query.interestRate});
+        } catch (e) {
+            response.status(500).render('pages/error', { error: e});
+        }
     } else {
-    	response.status(500).json({status: "error", "message": "You need provide all three parameters!"});
+        response.status(500).render('pages/error', { error: "You need provide all three parameters!"});
     }
 });
 
+app.post("/results/perMonthRetirementSavings", function (request, response) { 
+    if (request.body.param1 && request.body.param2 && request.body.param3) {
+        try {
+            var res = myData.retirementAmountIfSavingPerMonth(request.body.param1, request.body.param2, request.body.param3);
+            response.render('pages/result', { operationTitle: "Monthly Retirement Savings", result: res,
+                type: 1, param1: request.body.param1, param2:request.body.param2, param3:request.body.param3});
+        } catch (e) {
+            response.status(500).render('pages/error', { error: e});
+        }
+    } else {
+        response.status(500).render('pages/error', { error: "You need provide all three parameters!"});
+    }
+});
+
+app.post("/results/investedAmount", function (request, response) { 
+    if (request.body.param1 && request.body.param2 && request.body.param3) {
+        try {
+            var res = myData.investedAmountAfterSomeYears(request.body.param1, request.body.param2, request.body.param3);
+            response.render('pages/result', { operationTitle: "Invested Amount", result: res,
+                type: 2, param1: request.body.param1, param2:request.body.param2, param3:request.body.param3});
+        } catch (e) {
+            response.status(500).render('pages/error', { error: e});
+        }
+    } else {
+        response.status(500).render('pages/error', { error: "You need provide all three parameters!"});
+    }
+});
+
+app.post("/results/loanPayoff", function (request, response) { 
+    if (request.body.param1 && request.body.param2 && request.body.param3) {
+        try {
+            var res = myData.monthsToPayOffLoan(request.body.param1, request.body.param2, request.body.param3);
+            response.render('pages/result', { operationTitle: "Loan Payoff", result: res,
+                type: 3, param1: request.body.param1, param2:request.body.param2, param3:request.body.param3});
+        } catch (e) {
+            response.status(500).render('pages/error', { error: e});
+        }
+    } else {
+        response.status(500).render('pages/error', { error: "You need provide all three parameters!"});
+    }
+});
 
 // We can now navigate to localhost:3000
 app.listen(3000, function () {
